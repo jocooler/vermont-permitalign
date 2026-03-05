f585ff256626bd45c950ea5a900a13761259e0c8
Josiah Raiche <jjraiche@gmail.com>
2026-03-05 15:18:17 +0000

    File changes

--- src/pages/ProjectProfile.jsx
+++ src/pages/ProjectProfile.jsx
@@ -91,6 +91,7 @@ export default function ProjectProfile() {
   };
 
   const set = (k, v) => setForm(f => ({ ...f, [k]: v }));
+  const setProject = (k, v) => setProjectForm(f => ({ ...f, [k]: v }));
 
   if (loading) {
     return (
